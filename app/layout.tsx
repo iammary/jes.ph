dd526588aee77c66fc8a0d829fb65f10078c3c0c
Jes <mjanub@gmail.com>
2026-01-14 16:42:26 +1100

    chore: test vercel integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: 'Jes Anub | Software Engineer',
+  title: 'Jes | Software Engineer',
   description: 'Software engineer, frontend architect, based in Melbourne.',
 };
 
